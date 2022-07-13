7d3c5060cc3318e5f8bf602d9ecf00cf740b854c
zxhaxdr <12530685+yinzheng-zhong@users.noreply.github.com>
2022-07-13 19:48:25 +0100

    New method to get limited ec

--- tfgen/__init__.py
+++ tfgen/__init__.py
@@ -1,5 +1,6 @@
 from .tfgen import TFGen
 from .tfgen import InitialisingException
 from .observe_event_classes import get_observable_ec
+from .observe_event_classes import get_observable_ec_top_n
 from . import methods
 from . import models
--- tfgen/observe_event_classes.py
+++ tfgen/observe_event_classes.py
@@ -2,17 +2,48 @@ import pandas as pd
 import tfgen.utils as utils
 
 
-def get_observable_ec(attributes):
-    """
-    This function finds all observable event classes from an offline dataset.
-    :param attributes:
-    :return:
-    """
+def _convert_type(attributes):
     # check if attributes is a dataframe
     if isinstance(attributes, pd.DataFrame):
         attributes = list(attributes.astype(str).values)
     else:
         attributes = list(attributes)
 
-    event_classes = set([utils.convert_attr_to_ec(attribute) for attribute in attributes])
+    return attributes
+
+
+def get_observable_ec(attributes):
+    """
+    This function finds all observable event classes from an offline dataset.
+    :param attributes:
+    :return:
+    """
+    attributes = _convert_type(attributes)
+
+    num_attributes = len(attributes[0])
+    eot_string = utils.convert_attr_to_ec(['EOT'] * num_attributes)
+
+    event_classes = set([utils.convert_attr_to_ec(attribute) for attribute in attributes]) - {eot_string}
     return sorted(list(event_classes))
+
+
+def get_observable_ec_top_n(attributes, n=10):
+    """
+    This function finds all observable event classes from an offline dataset.
+    :param attributes:
+    :param n: number of top occurring event classes to return.
+    :return:
+    """
+    attributes = _convert_type(attributes)
+
+    num_attributes = len(attributes[0])
+    eot_string = utils.convert_attr_to_ec(['EOT'] * num_attributes)
+
+    event_classes = [utils.convert_attr_to_ec(attribute) for attribute in attributes]
+    keys = sorted(set(event_classes) - {eot_string})
+
+    counts = {key: event_classes.count(key) for key in keys}
+
+    top_ec = sorted(counts, key=counts.get, reverse=True)[:n]
+
+    return top_ec
